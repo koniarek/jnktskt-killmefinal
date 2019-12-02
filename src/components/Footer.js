b4d541abe09de898d1cf3cade59b899afcc28836
skwebarchitecture <szymon.koniarek@gmail.com>
2019-12-02 22:42:44 +0100

    Up footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import InstagramFeed from './InstagramFeed'
+import './Layout.js'
 import './Footer.css'
+import styled from '@emotion/styled'
+import { FaCopyright } from 'react-icons/fa'
+import Socials from './Socials'
 
-export default () => (
-  <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
-    <InstagramFeed count="8" />
-    <footer className="footer">
-      <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
-      </div>
-    </footer>
-  </div>
+const FooterWrap = styled.div`
+    background: black;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+`
+
+const SocialWrap = styled.div`
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 10px
+`
+
+const Footer = () => (
+	<FooterWrap>
+		<SocialWrap>
+			<Socials />
+		</SocialWrap>
+		<span>| millennium | 51 1160 2202 0000 0002 6070 5913 | tedoendoce | junkierap | sekta |</span>
+		<p>
+			<FaCopyright /> Copyright {new Date().getFullYear()}. junkierap.pl | All rights reserved. Developed by{' '}
+			<a href="https://skwebarchitecture.com/">Szymon Koniarek</a>.
+		</p>
+	</FooterWrap>
 )
+
+export default Footer
