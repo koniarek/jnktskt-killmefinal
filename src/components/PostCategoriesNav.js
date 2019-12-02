e4facecac0ebf1b2a295f8d67015a56436f8a95a
skwebarchitecture <szymon.koniarek@gmail.com>
2019-12-02 23:06:31 +0100

    Update

--- src/components/PostCategoriesNav.js
+++ src/components/PostCategoriesNav.js
@@ -11,6 +11,10 @@ const PostCategoriesNav = ({}) => <div className="PostCategoriesNav">
     className="NavLink"
     to='post-categories/news'>NEWS
   </Link>
+    <Link
+    className="NavLink"
+    to='pages/components'>JNKTSKT
+  </Link>
     <a href='https://jnktskt.onuniverse.com/'>JNKTSKT SKLEP</a>
   <Logo2/>
 </div>
