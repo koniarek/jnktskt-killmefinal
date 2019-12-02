88039e7f4f0d6fb0572e5d25c24ddc1210d980d1
skwebarchitecture <szymon.koniarek@gmail.com>
2019-12-02 23:09:06 +0100

    Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,103 +1,52 @@
 import React, { Component } from 'react'
-import { Location } from '@reach/router'
 import { Link } from 'gatsby'
-import { Menu, X } from 'react-feather'
 import Logo from './Logo'
-
+import Logo2 from './Logo2'
 import './Nav.css'
 
-export class Navigation extends Component {
-  state = {
-    active: false,
-    activeSubNav: false,
-    currentPath: false
-  }
+export default class Nav extends Component {
+	state = {
+		active: false
+	};
 
-  componentDidMount = () =>
-    this.setState({ currentPath: this.props.location.pathname })
+	handleMenuToggle = () => this.setState({ active: !this.state.active });
 
-  handleMenuToggle = () => this.setState({ active: !this.state.active })
+	// Only close nav if it is open
+	handleLinkClick = () => this.state.active && this.handleMenuToggle();
 
-  // Only close nav if it is open
-  handleLinkClick = () => this.state.active && this.handleMenuToggle()
+	render() {
+		const { active } = this.state;
 
-  toggleSubNav = subNav =>
-    this.setState({
-      activeSubNav: this.state.activeSubNav === subNav ? false : subNav
-    })
+		const NavLink = ({ className, children, ...props }) => (
+			<Link
+				{...props}
+				className={`NavLink ${className || ''}`}
+				onClick={this.handleLinkClick}
+			>
+				{children}
+			</Link>
+		);
 
-  render() {
-    const { active } = this.state,
-      { subNav } = this.props,
-      NavLink = ({ to, className, children, ...props }) => (
-        <Link
-          to={to}
-          className={`NavLink ${
-            to === this.state.currentPath ? 'active' : ''
-          } ${className}`}
-          onClick={this.handleLinkClick}
-          {...props}
-        >
-          {children}
-        </Link>
-      )
-
-    return (
-      <nav className={`Nav ${active ? 'Nav-active' : ''}`}>
-        <div className="Nav--Container container">
-          <Link to="/" onClick={this.handleLinkClick}>
-            <Logo />
-          </Link>
-          <div className="Nav--Links">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/components/">Components</NavLink>
-            <div
-              className={`Nav--Group ${
-                this.state.activeSubNav === 'posts' ? 'active' : ''
-              }`}
-            >
-              <span
-                className={`NavLink Nav--GroupParent ${
-                  this.props.location.pathname.includes('posts') ||
-                  this.props.location.pathname.includes('blog') ||
-                  this.props.location.pathname.includes('post-categories')
-                    ? 'active'
-                    : ''
-                }`}
-                onClick={() => this.toggleSubNav('posts')}
-              >
-                Blog
-              </span>
-              <div className="Nav--GroupLinks">
-                <NavLink to="/blog/" className="Nav--GroupLink">
-                  All Posts
-                </NavLink>
-                {subNav.posts.map((link, index) => (
-                  <NavLink
-                    to={link.slug}
-                    key={'posts-subnav-link-' + index}
-                    className="Nav--GroupLink"
-                  >
-                    {link.title}
-                  </NavLink>
-                ))}
-              </div>
-            </div>
-            <NavLink to="/default/">Default</NavLink>
-            <NavLink to="/contact/">Contact</NavLink>
-          </div>
-          <button
-            className="Button-blank Nav--MenuButton"
-            onClick={this.handleMenuToggle}
-          >
-            {active ? <X /> : <Menu />}
-          </button>
-        </div>
-      </nav>
-    )
-  }
+		return (
+			<nav className={`Nav ${active ? 'Nav-active' : ''}`}>
+				<div className="Nav--Container container">
+					<Link to="/" onClick={this.handleLinkClick}>
+						<Logo />
+					</Link>
+					<div className="Nav--Links">
+						<NavLink to="/" exact>
+							News
+						</NavLink>
+						<NavLink href="https://merchpage.netlify.com" exact>
+							Merch
+						</NavLink>
+						<NavLink href="https://sektapage.netlify.com" exact>
+							Jntskt
+						</NavLink>
+					</div>
+					<Logo2 />
+				</div>
+			</nav>
+		)
+	}
 }
-
-export default ({ subNav }) => (
-  <Location>{route => <Navigation subNav={subNav} {...route} />}</Location>
-)
